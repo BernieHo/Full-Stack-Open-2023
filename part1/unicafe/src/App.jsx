baf7ac17a76dc435ec95b35af0639fed03ddf37e
Bernhard Hobel <bernhard.hobel@gmx.at>
2023-08-31 23:13:58 +0200

    finish excercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -24,6 +24,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {good + neutral + bad > 0 ? (good - bad) / (good + neutral + bad) : 0}</p>
+      <p>positive {good + neutral + bad > 0 ? (good * 100) / (good + neutral + bad) : 0} %</p>
     </div>
   )
 }
