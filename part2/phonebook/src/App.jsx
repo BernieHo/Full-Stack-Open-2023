b144f51640f595f8f910e8aeac623b77bbfece4c
Bernhard Hobel <bernhard.hobel@gmx.at>
2023-09-06 23:49:04 +0200

    exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [shownPersons, setShownPersons] = useState(persons)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchInput, setSearchInput] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -12,12 +19,22 @@ const App = () => {
       return null
     }
     if (!newName || !newNumber) {
-      alert('Please fill out the form completely!')
+      alert('Please fill out name and number!')
       return null
     }
-    setPersons(persons.concat({ name: newName, number: newNumber }))
+    const updatedPersons = persons.concat({ name: newName, number: newNumber, id: persons.length + 1 })
+    setPersons(updatedPersons)
+    setShownPersons(filterPersons(updatedPersons, searchInput))
     setNewName('')
     setNewNumber('')
+  }
+
+  const filterPersons = (personList, searchString) => {
+    if (!searchString) {
+      return personList
+    } else {
+      return personList.filter((p) => p.name.toUpperCase().includes(searchString.toUpperCase()))
+    }
   }
 
   const handleNameInputChange = (event) => {
@@ -26,10 +43,19 @@ const App = () => {
   const handleNumberInputChange = (event) => {
     setNewNumber(event.target.value)
   }
+  const handleFilterInputChange = (event) => {
+    setSearchInput(event.target.value)
+    setShownPersons(filterPersons(persons, event.target.value))
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with <input value={searchInput} onChange={handleFilterInputChange} />
+        </div>
+      </form>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameInputChange} />
@@ -42,9 +68,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => {
+      {shownPersons.map((person) => {
         return (
-          <p key={person.name}>
+          <p key={person.id}>
             {person.name} {person.number}
           </p>
         )
