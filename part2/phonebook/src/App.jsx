373a3123907b12c5c3f99b24a66df50c5d37db53
Bernhard Hobel <bernhard.hobel@gmx.at>
2023-09-10 11:18:52 +0200

    finish exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,22 @@
 import Persons from './components/Persons'
 import FormNewPerson from './components/FormNewPerson'
 import Filter from './components/Filter'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [shownPersons, setShownPersons] = useState(persons)
+  const [persons, setPersons] = useState([])
+  const [shownPersons, setShownPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchInput, setSearchInput] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data)
+      setShownPersons(response.data)
+    })
+  }, [])
 
   const handleSubmit = (event) => {
     event.preventDefault()
